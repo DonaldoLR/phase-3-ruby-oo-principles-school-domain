5f0dee7b0dd7d1dd8bac77ad055312f6c9df6b92
github username <github email address>
2021-07-08 19:02:53 -0400

    practice writing classes in ruby and understanding more about OOP

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School 
+  
+  attr_accessor :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+  def grade(searching_grade)
+    @roster[searching_grade]
+  end
+  def sort
+    return @roster.each do |grade, student_array|
+      student_array.sort! {|first, second| first <=> second}
+    end
+  end
+end
